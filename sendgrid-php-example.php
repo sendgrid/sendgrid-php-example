8d1e185c301fef40ccf6b7022c8d97dd104b579c
scottmotte <scott@scottmotte.com>
2014-01-07 15:15:37 -0800

    example with turning off ssl verification

--- sendgrid-php-example.php
+++ sendgrid-php-example.php
@@ -6,7 +6,7 @@ $sendgrid_username = $_ENV['SENDGRID_USERNAME'];
 $sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
 $to                = $_ENV['TO'];
 
-$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password);
+$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
 $email    = new SendGrid\Email();
 $email->addTo($to)->
        setFrom($to)->
@@ -18,5 +18,5 @@ $email->addTo($to)->
        addMessageHeader('X-Transport', 'web')->
        addAttachment('./gif.gif', 'owl.gif');
 
-$response = $sendgrid->send($email);
+$response = $sendgrid->web->send($email);
 var_dump($response);
